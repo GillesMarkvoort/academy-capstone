1f20a7aefbff3ea4192747da2350df80bcff1393
GillesMarkvoort <gilles.markvoort@gmail.com>
2022-02-10 16:53:01 +0000

    flattened dataset

--- task1.py
+++ task1.py
@@ -5,7 +5,7 @@ import pyspark.sql.functions as psf
 from pyspark.sql import SparkSession
 
 
-# spark = (SparkSession.builder.config('spark.jars.packages', 'org.apache.hadoop:hadoop-aws:3.1.2')
+# spark = (SparkSession.builder.config('spark.jars.packages', 'org.apache.hadoop:hadoop-aws:3.1.2',)
 #     .config("fs.s3.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
 #     .config("fs.s3a.aws.credentials.provider", "com.amazonaws.auth.DefaultAWSCredentialsProviderChain")
 #     .getOrCreate()
@@ -13,9 +13,11 @@ from pyspark.sql import SparkSession
 
 #df = spark.read.json("s3://dataminded-academy-capstone-resources/raw/open_aq/")
 #df.write.format('json').save('save.json')
-spark = SparkSession.builder.getOrCreate()
+spark = (SparkSession.builder.config('spark.jars.packages', 
+ 'org.apache.hadoop:hadoop-aws:3.1.2,net.snowflake:spark-snowflake_2.12:2.9.0-spark_3.1,net.snowflake:snowflake-jdbc:3.13.3')
+ .getOrCreate())
 df = spark.read.json("save.json")
 df.show()
 df.printSchema()
-df.select("*", "coordinates.latitude","coordinates.longitude").show()
+df.select("*","coordinates.latitude","coordinates.longitude","date.local","date.utc").drop("coordinates","date").show()
 
